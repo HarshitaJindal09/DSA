70a17c5b05a564846dfded17430b7707c86894a4
Harshita Jindal <144919783+HarshitaJindal09@users.noreply.github.com>
2026-09-12 13:47:29 +0530

    Add Medium C++ Solution for 1749. Maximum Absolute Sum of Any Subarray from LeetCode

---
+++ LeetCode/Patterns/Dynamic Programming/1749. Maximum Absolute Sum of Any Subarray/Solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxAbsoluteSum(vector<int>& nums) {
+        int ans = nums[0];
+        int maxSum = nums[0];
+        int minSum = nums[0];
+
+        for(int i=1;i<nums.size();i++){
+            maxSum = max(nums[i], maxSum +nums[i]);
+            minSum = min(nums[i], minSum +nums[i]);
+
+            int absoluteSum = max(abs(maxSum), abs(minSum));
+            ans = max(ans,absoluteSum);
+        }
+        return ans;
+        
+    }
+};
