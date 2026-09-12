ededb25d5785c78bc3c457b80c0e26a587d728ac
Harshita Jindal <144919783+HarshitaJindal09@users.noreply.github.com>
2026-09-12 16:59:25 +0530

    Add Easy C++ Solution for 724. Find Pivot Index from LeetCode

--- LeetCode/C++/Easy/724. Find Pivot Index/Solution.cpp
+++ LeetCode/C++/Easy/724. Find Pivot Index/Solution.cpp
@@ -10,17 +10,16 @@ public:
         }
 
         int left = 0;
+
+        if(sum-nums[0] == 0){
+            return 0; 
+        }
         for(int i=1;i<nums.size();i++){
             left += nums[i-1];
             int right = sum-left-nums[i];
             if(left==right){
                 return i;
-            }
-            if(sum-nums[0] == 0){
-               return 0; 
-            }
-                
-            
+            }   
         } 
         return -1;   
     }
