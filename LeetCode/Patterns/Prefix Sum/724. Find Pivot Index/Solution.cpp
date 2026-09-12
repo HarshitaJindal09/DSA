d06127faaef78ebc77cb77efa240de854b089112
Harshita Jindal <144919783+HarshitaJindal09@users.noreply.github.com>
2026-09-12 16:56:28 +0530

    Add Easy C++ Solution for 724. Find Pivot Index from LeetCode

---
+++ LeetCode/Patterns/Prefix Sum/724. Find Pivot Index/Solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int sum = 0;
+// This does eventually create the total sum, but only as you move through the loop.
+//The cleaner approach is to calculate the total sum first.
+
+        for(int x:nums){
+            sum+= x;
+        }
+
+        int left = 0;
+        for(int i=1;i<nums.size();i++){
+            left += nums[i-1];
+            int right = sum-left-nums[i];
+            if(left==right){
+                return i;
+            }
+            if(sum-nums[0] == 0){
+               return 0; 
+            }
+                
+            
+        } 
+        return -1;   
+    }
+};
